ebeee7619280618649294519e0b44595a36ebd04
gilang aditya <madegilangaditya32@gmail.com>
2022-08-18 23:41:34 +0800

    add alert when input are blank

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 console.log('masuk');
+const inp = document.querySelectorAll('#card-submit-form input');
+
+inp.forEach((item)=>{
+    item.addEventListener('focusout', inputChecker);
+})
+
+function inputChecker(event){
+    if(event.target.value == ''){
+        event.target.parentElement.classList.add('error');
+        event.target.parentElement.nextElementSibling ? event.target.parentElement.nextElementSibling.textContent = "Can't be blank" : event.target.parentElement.insertAdjacentHTML("afterend",
+        "<span class='error-msg'>Can't be blank</span>");
+        
+    }else{
+        event.target.parentElement.classList.remove('error');
+        event.target.parentElement.nextElementSibling.remove();
+    }
+}
