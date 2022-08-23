9c2f8476b6b241cc2641e3e8394968a7a96cae9f
gilang aditya <madegilangaditya32@gmail.com>
2022-08-23 19:51:48 +0800

    add button disable and enable validation jd

--- js/app.js
+++ js/app.js
@@ -1,56 +1,53 @@
 console.log('masuk');
 const inp = document.querySelectorAll('#card-submit-form input');
 const cardNumber = document.querySelector('#card-number');
+const cardName = document.querySelector('#card-name');
+const cardMonth = document.querySelector('#card-month');
+const cardYear = document.querySelector('#card-year');
+const cardCvc = document.querySelector('#card-cvc');
 const btn = document.querySelector('#card-submit-form .btn');
-let i = 0;
 
 inp.forEach((item, index)=>{
-    disableBtn(item,index);
-    // cardNumber.addEventListener('focusout', numberOnly);
     item.addEventListener('focusout', inputChecker);
 })
-
-// btn.addEventListener('click', ()=>{
-    
-//     inp.forEach((item)=>{
-//         if(item.parentElement.classList.contains('error') && d === 1){
-//             btn.classList.add('disabled');
-//         }else if(d === 0){
-//             btn.classList.remove('disabled');
-//         }
-//     })
-//     console.log(d);
-// });
 
 
 
 function inputChecker(event){
+    console.log(cardNumber.value);
+    console.log(cardName.value);
+    console.log(cardMonth.value);
+    console.log(cardYear.value);
+    console.log(cardCvc.value);
+    
     let numbers = /^[0-9]+$/;
     if(event.target.value == ''){
         event.target.parentElement.classList.add('error');
         event.target.parentElement.nextElementSibling ? event.target.parentElement.nextElementSibling.textContent = "Can't be blank" : event.target.parentElement.insertAdjacentHTML("afterend",
         "<span class='error-msg'>Can't be blank</span>");
-        btn.setAttribute('disabled','disabled'); 
+        disableBtn();
     }else if((event.target.id == 'card-number' && !event.target.value.match(numbers)) || (event.target.id == 'card-month' && !event.target.value.match(numbers)) || (event.target.id == 'card-year' && !event.target.value.match(numbers)) || (event.target.id == 'card-cvc' && !event.target.value.match(numbers))){
         event.target.parentElement.classList.add('error');
         event.target.parentElement.nextElementSibling ? event.target.parentElement.nextElementSibling.textContent = "Wrong format, numbers only" : event.target.parentElement.insertAdjacentHTML("afterend",
         "<span class='error-msg'>Wrong format, numbers only</span>");
-        btn.setAttribute('disabled','disabled');  
+        disableBtn();  
     }else if(event.target.parentElement.classList.contains('error')){ 
+        
         event.target.parentElement.classList.remove('error');
         event.target.parentElement.nextElementSibling.remove();
-        btn.removeAttribute('disabled');
+        disableBtn();
+        
+    }else{
+        disableBtn();
     }
+   
 }
 
-function disableBtn(item, index){
-    
-    
-    console.log(index);
-    console.log(i);
-    if(item.value == '' && i == index - 1  ){
+function disableBtn(){
+    if(cardNumber.parentElement.classList.contains('error') || cardName.parentElement.classList.contains('error') || cardMonth.parentElement.classList.contains('error') || cardYear.parentElement.classList.contains('error') || cardCvc.parentElement.classList.contains('error')){ 
         btn.setAttribute('disabled','disabled');
-        i++;
-        console.log(item.value);
+    }else if(cardNumber.value != '' && cardName.value != '' && cardMonth.value != '' && cardYear.value != '' && cardCvc.value != '' ){
+        btn.removeAttribute('disabled');
+        console.log('test 1');
     }
 }
